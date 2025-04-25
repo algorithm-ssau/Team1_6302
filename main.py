f6113187e525551711ae59ea05adc1a2c0fe488c
diasto3 <91277548+diasto3@users.noreply.github.com>
2025-04-25 23:29:08 +0400

    сервер
    добавление базового сервера

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+from flask import Flask, render_template, send_from_directory, redirect, url_for
+
+app = Flask(__name__, static_folder=".", template_folder=".")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index_html():
+    return render_template('index.html')
+
+@app.route('/catalog.html')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
