55fa0cf220c40945d398765475f26234f6b73c9d
MOTILDA228 <matveymessi32@gmail.com>
2025-05-10 20:03:01 +0400

    Добавлены новые велосипеды в seeder.js

--- server/seeder.js
+++ server/seeder.js
@@ -69,6 +69,61 @@ const initDB = async () => {
         inStock: true,
         rating: 4.2,
         numReviews: 15
+      },
+      {
+        name: 'Электровелосипед Urban E-Rider',
+        category: 'electric',
+        price: 85000,
+        description: 'Современный электровелосипед для городской среды. Мощный двигатель, большой запас хода и стильный дизайн.',
+        image: 'img/electric_bike.jpg',
+        frameSize: 'M',
+        inStock: true,
+        rating: 4.7,
+        numReviews: 23
+      },
+      {
+        name: 'BMX Freestyle Pro',
+        category: 'bmx',
+        price: 30000,
+        description: 'Профессиональный BMX велосипед для выполнения трюков. Прочная рама, усиленные колеса и отличная маневренность.',
+        image: 'img/bmx_bike.jpg',
+        frameSize: 'S',
+        inStock: true,
+        rating: 4.9,
+        numReviews: 18
+      },
+      {
+        name: 'Складной велосипед Folding Master',
+        category: 'folding',
+        price: 32000,
+        description: 'Компактный складной велосипед для путешествий и ежедневных поездок. Легко складывается и помещается в багажник автомобиля.',
+        image: 'img/folding_bike.jpg',
+        frameSize: 'Universal',
+        inStock: true,
+        rating: 4.3,
+        numReviews: 9
+      },
+      {
+        name: 'Гравийный велосипед Gravel Explorer',
+        category: 'gravel',
+        price: 65000,
+        description: 'Универсальный гравийный велосипед для дальних поездок по различным типам дорог. Прочная рама и широкие покрышки.',
+        image: 'img/gravel_bike.jpg',
+        frameSize: 'L',
+        inStock: true,
+        rating: 4.6,
+        numReviews: 14
+      },
+      {
+        name: 'Трековый велосипед Track Speed',
+        category: 'track',
+        price: 58000,
+        description: 'Профессиональный трековый велосипед для велодрома. Фиксированная передача, легкая аэродинамическая рама.',
+        image: 'img/track_bike.jpg',
+        frameSize: 'M',
+        inStock: true,
+        rating: 4.8,
+        numReviews: 7
       }
     ];
     
