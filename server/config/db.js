b4abfeded2ee926267ec28a8a4dec832e80ebd4d
diasto3 <91277548+diasto3@users.noreply.github.com>
2025-05-14 14:17:57 +0400

    change db connect

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/velopro', {
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/velopro';
+    const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
